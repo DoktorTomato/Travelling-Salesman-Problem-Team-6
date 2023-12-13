93a2dc63b0b86f86114423dcd4b1b8f5536b1604
Ivan Shevchuk <vanya2006shevchuk@gmail.com>
2023-12-13 09:42:34 +0200

    precise_alg function done, need to test more

--- tsp_project.py
+++ tsp_project.py
@@ -1,7 +1,6 @@
 '''
 В цьому модулі знаходяться функції допомагають та вирішують задачу комівояжера
 '''
-import itertools as it
 from copy import deepcopy
 
 def read_file(file_path:str):
@@ -59,61 +58,10 @@ f,g,9')
         new_dict[key] = [(i[0], int(i[1])) for i in value]
     return new_dict
 
-def all_subsets(gen_set:set)->list:
-    '''
-    Ця функція знаходить всі можливі підмножини даної множини (задана списком)
-    і повертає їх список
-    >>> all_subsets(['b', 'c', 'd', 'e', 'f', 'g'])
-    [('b',), ('c',), ('d',), ('e',), ('f',), ('g',), \
-('b', 'c'), ('b', 'd'), ('b', 'e'), ('b', 'f'), ('b', 'g'), \
-('c', 'd'), ('c', 'e'), ('c', 'f'), ('c', 'g'), ('d', 'e'), \
-('d', 'f'), ('d', 'g'), ('e', 'f'), ('e', 'g'), ('f', 'g'), \
-('b', 'c', 'd'), ('b', 'c', 'e'), ('b', 'c', 'f'), ('b', 'c', 'g'), \
-('b', 'd', 'e'), ('b', 'd', 'f'), ('b', 'd', 'g'), ('b', 'e', 'f'), \
-('b', 'e', 'g'), ('b', 'f', 'g'), ('c', 'd', 'e'), ('c', 'd', 'f'), \
-('c', 'd', 'g'), ('c', 'e', 'f'), ('c', 'e', 'g'), ('c', 'f', 'g'), \
-('d', 'e', 'f'), ('d', 'e', 'g'), ('d', 'f', 'g'), ('e', 'f', 'g'), \
-('b', 'c', 'd', 'e'), ('b', 'c', 'd', 'f'), ('b', 'c', 'd', 'g'), \
-('b', 'c', 'e', 'f'), ('b', 'c', 'e', 'g'), ('b', 'c', 'f', 'g'), \
-('b', 'd', 'e', 'f'), ('b', 'd', 'e', 'g'), ('b', 'd', 'f', 'g'), \
-('b', 'e', 'f', 'g'), ('c', 'd', 'e', 'f'), ('c', 'd', 'e', 'g'), \
-('c', 'd', 'f', 'g'), ('c', 'e', 'f', 'g'), ('d', 'e', 'f', 'g'), \
-('b', 'c', 'd', 'e', 'f'), ('b', 'c', 'd', 'e', 'g'), \
-('b', 'c', 'd', 'f', 'g'), ('b', 'c', 'e', 'f', 'g'), \
-('b', 'd', 'e', 'f', 'g'), ('c', 'd', 'e', 'f', 'g')]
-    >>> all_subsets(['b', 'c', 'd'])
-    [('b',), ('c',), ('d',), ('b', 'c'), ('b', 'd'), ('c', 'd')]
-    '''
-    res_lst = []
-    for i in range(1, len(gen_set)):
-        res_lst.extend(list(it.combinations(gen_set, i)))
-    return res_lst
+memo = {} #Пам'ять про вже пройдені шляхи
 
-def ispath(graph:dict, start:str | int, destination:str | int)->bool:
-    '''
-    Ця функція приймає на вхід словник з даними про шляхи який можна створити
-    за допомогою функції read_csv, початкову точку та ціль.
-    На вихід ця функція повертає True чи False взалежності від того чи існує такий
-    шлях між даними точками
-    >>> ispath({'a': [('b', 12), ('c', 10), ('g', 12)], \
-'b': [('d', 12), ('c', 8), ('a', 12)], 'c': [('b', 8), ('a', 10), \
-('g', 9), ('e', 3), ('d', 11)], 'g': [('a', 12), ('c', 9), ('e', 7), ('f', 9)], \
-'e': [('g', 7), ('c', 3), ('d', 11), ('f', 6)], 'd': [('e', 11), ('b', 12), \
-('c', 11), ('f', 10)], 'f': [('e', 6), ('d', 10), ('g', 9)]}, 'a', 'c')
-    True
-    >>> ispath({'a': [('b', 12), ('c', 10), ('g', 12)], \
-'b': [('d', 12), ('c', 8), ('a', 12)], 'c': [('b', 8), ('a', 10), \
-('g', 9), ('e', 3), ('d', 11)], 'g': [('a', 12), ('c', 9), ('e', 7), ('f', 9)], \
-'e': [('g', 7), ('c', 3), ('d', 11), ('f', 6)], 'd': [('e', 11), ('b', 12), \
-('c', 11), ('f', 10)], 'f': [('e', 6), ('d', 10), ('g', 9)]}, 'a', 'f')
-    False
-    '''
-    for route in graph[start]:
-        if route[0] == destination:
-            return True
-    return False
-
-def precise_alg(graph:dict, start:str | int, dest:str | int, go_through:list, memo:dict)->tuple:
+def precise_alg(graph:dict, start:str | int, dest:str | int,
+                go_through:list, flag:bool = True)->tuple:
     '''
     Ця функція вирішує задачу комівояжера точним алгоритмом (Held-Karp algorythm)
     Ця функція повертає кортеж значень з яких перше значення це довжина (вага) шляху,
@@ -122,15 +70,34 @@ def precise_alg(graph:dict, start:str | int, dest:str | int, go_through:list, me
 'b': [('a', 2), ('c', 7), ('d', 3)], \
 'c': [('a', 15), ('b', 7), ('d', 12)], \
 'd': [('a', 6), ('b', 3), ('c', 12)]}, 'a', 'a', \
-['b', 'c', 'd'], {})
+['b', 'c', 'd'])
     (27, ['a', 'd', 'c', 'b', 'a'])
     >>> precise_alg({'a': [('b', 10), ('c', 15), ('d', 20)], 'b': [('a', 10),\
 ('c', 35), ('d', 25)], 'c': [('a', 15), ('b', 35), ('d', 30)],\
 'd': [('a', 20), ('b', 25), ('c', 30)]}, 'a', 'a', \
-['b', 'c', 'd'], {})
+['b', 'c', 'd'])
     (80, ['a', 'c', 'd', 'b', 'a'])
+    >>> precise_alg({'a': [('b', 12), ('c', 10), ('g', 12)], \
+'b': [('d', 12), ('c', 8), ('a', 12)], 'c': [('b', 8), ('a', 10), \
+('g', 9), ('e', 3), ('d', 11)], 'g': [('a', 12), ('c', 9), ('e', 7), ('f', 9)], \
+'e': [('g', 7), ('c', 3), ('d', 11), ('f', 6)], 'd': [('e', 11), ('b', 12), \
+('c', 11), ('f', 10)], 'f': [('e', 6), ('d', 10), ('g', 9)]}, 'a', 'a',\
+['b', 'c', 'd', 'e', 'f', 'g'])
+    (63, ['a', 'c', 'e', 'g', 'f', 'd', 'b', 'a'])
     '''
-    path = []
+    # Підготовчі роботи
+    ## Якщо функція запущена вперше то пам'ять про шляхи очищується
+    if flag:
+        memo.clear()
+    ## Заміна неіснуючих ребер на ребра з довжиною inf
+    for vertex in graph:
+        pos_to_go = [x[0] for x in graph[vertex]]
+        for vert in graph:
+            if vert != vertex:
+                if vert not in pos_to_go:
+                    graph[vertex].append((vert, float('inf')))
+
+    path = [] #Найвигідніший шлях поданий списком
     the_way = str((start, dest, go_through))
     # base case
     if len(go_through) < 1:
@@ -153,11 +120,11 @@ def precise_alg(graph:dict, start:str | int, dest:str | int, go_through:list, me
             routes = []
             path = []
             for city in go_through:
-                val1 = precise_alg(graph, city, dest, [], memo)[0]
+                val1 = precise_alg(graph, city, dest, [])[0]
                 subset_needed = [x for x in go_through if x != city]
-                val2 = precise_alg(graph, start, city, subset_needed, memo)[0]
+                val2 = precise_alg(graph, start, city, subset_needed, False)[0]
                 path.append(start)
-                path.extend(precise_alg(graph, start, city, subset_needed, memo)[1])
+                path.extend(precise_alg(graph, start, city, subset_needed, False)[1])
                 path.append(city)
                 path_copy = deepcopy(path)
                 path.clear()
